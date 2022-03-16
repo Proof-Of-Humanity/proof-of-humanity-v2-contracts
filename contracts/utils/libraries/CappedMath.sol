5abe3ac26539d63f390c77744b150c73cd5c6b8f
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2022-03-16 20:02:20 +0200

    fix: fixed CappedMath for 0.8

--- contracts/utils/libraries/CappedMath.sol
+++ contracts/utils/libraries/CappedMath.sol
@@ -1,10 +1,10 @@
-//SPDX-License-Identifier: MIT
 /**
- *  @authors: [@mtsalenc]
- *  @reviewers: [@shalzz]
- *  @auditors: []
- *  @bounties: []
- *  @deployments: []
+ * @authors: [@mtsalenc*, @hbarcelos*, @fnanni-0]
+ * @reviewers: [@clesaege*, @ferittuncer*]
+ * @auditors: []
+ * @bounties: []
+ * @deployments: []
+ * SPDX-License-Identifier: MIT
  */
 
 pragma solidity ^0.8;
@@ -14,15 +14,16 @@ pragma solidity ^0.8;
  * @dev Math operations with caps for under and overflow.
  */
 library CappedMath {
-    uint256 private constant UINT_MAX = 2**256 - 1;
-    uint64 private constant UINT64_MAX = 2**64 - 1;
+    uint256 private constant UINT_MAX = type(uint256).max;
 
     /**
      * @dev Adds two unsigned integers, returns 2^256 - 1 on overflow.
      */
     function addCap(uint256 _a, uint256 _b) internal pure returns (uint256) {
-        uint256 c = _a + _b;
-        return c >= _a ? c : UINT_MAX;
+        unchecked {
+            uint256 c = _a + _b;
+            return c >= _a ? c : UINT_MAX;
+        }
     }
 
     /**
@@ -42,24 +43,9 @@ library CappedMath {
         // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
         if (_a == 0) return 0;
 
-        uint256 c = _a * _b;
-        return c / _a == _b ? c : UINT_MAX;
-    }
-
-    function addCap64(uint64 _a, uint64 _b) internal pure returns (uint64) {
-        uint64 c = _a + _b;
-        return c >= _a ? c : UINT64_MAX;
-    }
-
-    function subCap64(uint64 _a, uint64 _b) internal pure returns (uint64) {
-        if (_b > _a) return 0;
-        else return _a - _b;
-    }
-
-    function mulCap64(uint64 _a, uint64 _b) internal pure returns (uint64) {
-        if (_a == 0) return 0;
-
-        uint64 c = _a * _b;
-        return c / _a == _b ? c : UINT64_MAX;
+        unchecked {
+            uint256 c = _a * _b;
+            return c / _a == _b ? c : UINT_MAX;
+        }
     }
 }
