2cde8637b1678c01f68e6ec936c7423243cd9a3c
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2022-03-23 15:08:49 +0200

    fix: CappedMath for uint64

--- contracts/utils/libraries/CappedMath.sol
+++ contracts/utils/libraries/CappedMath.sol
@@ -15,6 +15,7 @@ pragma solidity ^0.8;
  */
 library CappedMath {
     uint256 private constant UINT_MAX = type(uint256).max;
+    uint64 private constant UINT64_MAX = type(uint64).max;
 
     /**
      * @dev Adds two unsigned integers, returns 2^256 - 1 on overflow.
@@ -46,6 +47,30 @@ library CappedMath {
         unchecked {
             uint256 c = _a * _b;
             return c / _a == _b ? c : UINT_MAX;
+        }
+    }
+
+    function addCap64(uint64 _a, uint64 _b) internal pure returns (uint64) {
+        unchecked {
+            uint64 c = _a + _b;
+            return c >= _a ? c : UINT64_MAX;
+        }
+    }
+
+    function subCap64(uint64 _a, uint64 _b) internal pure returns (uint64) {
+        if (_b > _a) return 0;
+        else return _a - _b;
+    }
+
+    function mulCap64(uint64 _a, uint64 _b) internal pure returns (uint64) {
+        // Gas optimization: this is cheaper than requiring '_a' not being zero, but the
+        // benefit is lost if '_b' is also tested.
+        // See: https://github.com/OpenZeppelin/openzeppelin-solidity/pull/522
+        if (_a == 0) return 0;
+
+        unchecked {
+            uint64 c = _a * _b;
+            return c / _a == _b ? c : UINT64_MAX;
         }
     }
 }
