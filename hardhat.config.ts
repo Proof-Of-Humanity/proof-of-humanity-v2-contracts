304c19195fc2509089e3595ad4a3550956bbf3e4
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2022-11-28 11:39:41 -0500

    Change pragma in config

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,22 +1,42 @@
 import * as dotenv from "dotenv";
-import { HardhatUserConfig } from "hardhat/config";
+import { HardhatUserConfig, task } from "hardhat/config";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
+import "@nomiclabs/hardhat-etherscan";
 import "solidity-coverage";
 import "@openzeppelin/hardhat-upgrades";
 import "hardhat-contract-sizer";
+import "hardhat-deploy";
 
 dotenv.config();
 
+// task("Accounts", "Prints the accounts", async (_taskArgs, hre) => {
+//   console.log(hre.getChainId());
+//   console.log(hre.getNamedAccounts());
+// });
+
 const config: HardhatUserConfig = {
-  solidity: { version: "0.8.14", settings: { optimizer: { enabled: true, runs: 1800 } } },
+  solidity: { version: "0.8.16", settings: { optimizer: { enabled: true, runs: 10000 }, viaIR: true } },
   networks: {
-    hardhat: { allowUnlimitedContractSize: true, chainId: 1 },
-    sokol: { chainId: 77, url: "https://sokol.poa.network/" },
-    xdai: { chainId: 100, url: "https://rpc.xdaichain.com/" },
-    kovan: { chainId: 42, url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY!}` },
-    mainnet: { chainId: 1, url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY!}` },
+    hardhat: { chainId: 1, allowUnlimitedContractSize: true },
+    // mainnet: { chainId: 1, url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY!}` },
+    gnosis: {
+      chainId: 100,
+      url: `https://rpc.gnosischain.com/`,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    goerli: {
+      chainId: 5,
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY!}`,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
+    "optimism-goerli": {
+      chainId: 420,
+      url: `https://goerli.optimism.io`,
+      accounts: [process.env.PRIVATE_KEY!],
+    },
   },
+  etherscan: { apiKey: process.env.ETHERSCAN_API_KEY },
 };
 
 export default config;
