389c26302ff4ca6ab7bb1ae9f8521f56eb1068bb
Andrei Radu <47434163+AndreiMVP@users.noreply.github.com>
2023-03-06 13:25:53 -0300

    update gateways

--- contracts/bridge-gateways/AMBBridgeGateway.sol
+++ contracts/bridge-gateways/AMBBridgeGateway.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity 0.8.18;
 
 import {IAMB} from "./IAMB.sol";
 import {IBridgeGateway} from "./IBridgeGateway.sol";
@@ -7,20 +7,26 @@ import {IBridgeGateway} from "./IBridgeGateway.sol";
 contract AMBBridgeGateway is IBridgeGateway {
     IAMB public immutable amb;
     address public immutable homeProxy;
+    address public foreignGateway;
 
     constructor(IAMB _amb, address _homeProxy) {
         amb = _amb;
         homeProxy = _homeProxy;
     }
 
+    function setForeignGateway(address _foreignGateway) public {
+        require(foreignGateway == address(0x0), "Foreign proxy already set");
+        foreignGateway = _foreignGateway;
+    }
+
     function sendMessage(bytes memory _data) external override {
         require(msg.sender == homeProxy, "Only home proxy allowed");
-        amb.requireToPassMessage(address(this), abi.encodeCall(this.receiveMessage, (_data)), amb.maxGasPerTx());
+        amb.requireToPassMessage(foreignGateway, abi.encodeCall(this.receiveMessage, (_data)), amb.maxGasPerTx());
     }
 
     function receiveMessage(bytes memory _data) external override {
         require(msg.sender == address(amb), "Only AMB allowed");
-        require(amb.messageSender() == address(this), "AMB sender must have address as this contract");
+        require(amb.messageSender() == foreignGateway, "AMB sender must have address as this contract");
         homeProxy.call(_data);
     }
 }
--- contracts/bridge-gateways/OptimismBridgeGateway.sol
+++ contracts/bridge-gateways/OptimismBridgeGateway.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.17;
+pragma solidity 0.8.18;
 
 import {IBridgeGateway} from "./IBridgeGateway.sol";
 import {ICrossDomainMessenger} from "./ICrossDomainMessenger.sol";
@@ -7,28 +7,28 @@ import {ICrossDomainMessenger} from "./ICrossDomainMessenger.sol";
 contract OptimismBridgeGateway is IBridgeGateway {
     uint32 internal constant GAS_LIMIT = 5_000_000;
 
-    ICrossDomainMessenger public immutable homeMessenger;
+    ICrossDomainMessenger public immutable messenger;
     address public immutable homeProxy;
-    address public foreignProxy;
+    address public foreignGateway;
 
     constructor(address _messenger, address _homeProxy) {
-        homeMessenger = ICrossDomainMessenger(_messenger);
+        messenger = ICrossDomainMessenger(_messenger);
         homeProxy = _homeProxy;
     }
 
-    function setForeignProxy(address _foreignProxy) public {
-        require(foreignProxy == address(0x0), "Foreign proxy already set");
-        foreignProxy = _foreignProxy;
+    function setForeignGateway(address _foreignGateway) public {
+        require(foreignGateway == address(0x0), "Foreign proxy already set");
+        foreignGateway = _foreignGateway;
     }
 
     function sendMessage(bytes memory _data) external override {
         require(msg.sender == homeProxy, "Only home proxy allowed");
-        homeMessenger.sendMessage(foreignProxy, abi.encodeCall(this.receiveMessage, (_data)), GAS_LIMIT);
+        messenger.sendMessage(foreignGateway, abi.encodeCall(this.receiveMessage, (_data)), GAS_LIMIT);
     }
 
     function receiveMessage(bytes memory _data) external override {
-        require(msg.sender == address(homeMessenger), "Only home messenger allowed");
-        require(homeMessenger.xDomainMessageSender() == foreignProxy, "Message sender must be foreign proxy");
+        require(msg.sender == address(messenger), "Only home messenger allowed");
+        require(messenger.xDomainMessageSender() == foreignGateway, "Message sender must be foreign proxy");
         homeProxy.call(_data);
     }
 }
