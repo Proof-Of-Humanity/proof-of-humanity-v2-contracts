3588ed625c08006811f85d384172f3da6c71a59a
salgozino <salgozino@gmail.com>
2024-08-13 09:58:31 +0200

    fix: add default rpc if no api key

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,12 +1,12 @@
-import * as dotenv from "dotenv";
-import { extendEnvironment, HardhatUserConfig, task } from "hardhat/config";
-import "@typechain/hardhat";
 import "@nomicfoundation/hardhat-toolbox";
-import "solidity-coverage";
 import "@openzeppelin/hardhat-upgrades";
+import "@typechain/hardhat";
+import * as dotenv from "dotenv";
 import "hardhat-contract-sizer";
 import "hardhat-deploy";
 import "hardhat-deploy-ethers";
+import { extendEnvironment, HardhatUserConfig, task } from "hardhat/config";
+import "solidity-coverage";
 
 dotenv.config();
 
@@ -23,11 +23,13 @@ const config: HardhatUserConfig = {
   solidity: { version: "0.8.20", settings: { optimizer: { enabled: true, runs: 3200 }, viaIR: true } },
   networks: {
     hardhat: { chainId: 1, allowUnlimitedContractSize: true },
-    mainnet : {
+    mainnet: {
       chainId: 1,
       //url: `https://mainnet.gateway.tenderly.co/${process.env.TENDERLY_API_KEY!}`,
       //url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY!}`,
-      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY_PRIVATE!}`,
+      url: process.env.INFURA_API_KEY_PRIVATE
+        ? `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY_PRIVATE!}`
+        : "https://eth.llamarpc.com",
       accounts: [process.env.PRIVATE_KEY!],
     },
     gnosis: {
@@ -43,7 +45,9 @@ const config: HardhatUserConfig = {
     }, */
     sepolia: {
       chainId: 11155111,
-      url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY_PRIVATE!}`,
+      url: process.env.INFURA_API_KEY_PRIVATE
+        ? `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY_PRIVATE}`
+        : "https://ethereum-sepolia-rpc.publicnode.com",
       accounts: [process.env.PRIVATE_KEY!],
     },
   },
@@ -54,7 +58,8 @@ const config: HardhatUserConfig = {
       xdai: `${process.env.XDAI_API_KEY!}`,
       //chiado: `${process.env.CHIADO_API_KEY!}`,
     },
-    customChains: [ // This needs to be commented before deploying and only serves for verifying
+    customChains: [
+      // This needs to be commented before deploying and only serves for verifying
       /* {
         network: "chiado",
         chainId: 10200,
@@ -72,9 +77,9 @@ const config: HardhatUserConfig = {
           apiURL: `https://gnosis.blockscout.com/api`,
           browserURL: `https://blockscout.gnosischain.net`,
         },
-      }
-    ]
-  }
+      },
+    ],
+  },
 };
 
 export default config;
