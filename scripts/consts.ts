56dfec722cbdc395f535f19efa459137027b09b5
Martin <martin.moguillansky@gmail.com>
2024-09-04 13:16:41 -0300

    updated policy ipfs links into registration and clearing meta ev

--- scripts/consts.ts
+++ scripts/consts.ts
@@ -2,8 +2,9 @@ import { formatEther } from "ethers";
 import { ethers } from "hardhat";
 import { isTestnet, SUPPORTED_NETWORKS } from "./consts/chains/chains";
 
-export const REGISTRATION_META_EVIDENCE = "/ipfs/QmadJhyPxhk5AYrdE6JMwhC7TpsA47YZwFP28VKkr1ffJF";
-export const CLEARING_META_EVIDENCE = "/ipfs/QmRqKmjVk1FcCRcTnuZmMG6SZEBB9LkUJb7Z4SVhJGHEfw";
+// Policy referred for deployments: /ipfs/QmTWdrNJ1KBsnNcMq5PeZQyvJfnwJexvDwnbLqZsVh2L21
+export const REGISTRATION_META_EVIDENCE = "/ipfs/QmSRrYV3vg5q6CFpZDQgwSo6dPe81V5XBhST47G3aQYU5H";
+export const CLEARING_META_EVIDENCE = "/ipfs/QmTCbojsdDDGvtGFmLj46QtfBbkNhMbgfu8VSmsx7TznSj";
 export const POH_V1_Address = "0xC5E9dDebb09Cd64DfaCab4011A0D5cEDaf7c9BDb"; // PoH v1
 export const NULL_Address = "0x";
 
